35d23149ef62e75e0725458bc978f368581269d4
mguadagni <guadagnimichael@gmail.com>
2022-06-16 21:09:16 -0400

    Created basic react framework

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+
+function App() {
+
+  const styles = {
+    appDiv: {textAlign: 'center'},
+    header: {marginTop: '3vh'},
+    primaryDiv: {
+      width: '100vw', height: '90vh',
+      display: 'flex', flexDirection: 'row',
+      borderTop: '1px solid black'
+    },
+    formDiv: {
+      flex: 3, borderRight: '1px solid black',
+      display: 'flex', flexDirection: 'column',
+      padding: '0 10px'
+    },
+    dataDiv: {flex: 5, padding: '0 10px'},
+  }
+
+  return (
+    <div style={styles.appDiv}>
+      <h1 style={styles.header}> Stocks Data</h1>
+        <div style={styles.primaryDiv}>
+          <div style={styles.formDiv}/>
+          <div style={styles.dataDiv}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
